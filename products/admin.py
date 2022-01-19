b0a718584cbb57e24fbbcb9653ad01e34d414e4c
Greg <grzegorz.niespodzianek@gmail.com>
2022-01-19 19:40:18 +0100

    Initial commit

---
+++ products/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import *
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("category", "name", "price", "description", )
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ("product", "color", "size", )
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("cost", "created", )
+
+@admin.register(OrderPosition)
+class OrderPositionAdmin(admin.ModelAdmin):
+    list_display = ("product_type", "order", "product", "amount", )
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("order", "status")
---
+++ products/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Product(models.Model):
+
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=255)
+
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ProductType(models.Model):
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+
+    color = models.CharField(max_length=255)
+
+    size = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.color, self.size}"
+
+
+class Order(models.Model):
+    cost = models.DecimalField(max_digits=8, decimal_places=2)
+
+    surname = models.CharField(max_length=255)
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.surname} - {self.created}"
+
+
+class OrderPosition(models.Model):
+    product_type = models.ForeignKey("ProductType", on_delete=models.CASCADE)
+
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+
+    amount = models.IntegerField()
+
+    product = models.ForeignKey("Product", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product, self.amount}"
+
+
+class Payment(models.Model):
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+
+    status = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.order} - {self.status}"
+
+
+
+
